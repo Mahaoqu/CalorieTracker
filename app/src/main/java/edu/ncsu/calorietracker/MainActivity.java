02016c8965a0c485f6e20c3b07bfe4d429c80b15
Rundi Liu <15775677930@163.com>
2021-09-16 15:45:05 -0400

    add TextView and a button

--- app/src/main/java/edu/ncsu/calorietracker/MainActivity.java
+++ app/src/main/java/edu/ncsu/calorietracker/MainActivity.java
@@ -2,13 +2,27 @@ package edu.ncsu.calorietracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mbt_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.mbt_textView = (Button) findViewById(R.id.bt_textview);
+
+        this.mbt_textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // jump to new page (TextView)
+                Intent i = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/ncsu/calorietracker/TextViewActivity.java
@@ -0,0 +1,17 @@
+package edu.ncsu.calorietracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class TextViewActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_view);
+
+    }
+}
