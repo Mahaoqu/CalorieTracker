cf84c7cd7a2f8ac1aaac6f54749cd414c1a5df3f
Rundi Liu <15775677930@163.com>
2021-09-17 18:02:57 -0400

    Test one line

--- app/src/main/java/edu/ncsu/calorietracker/TextViewActivity.java
+++ app/src/main/java/edu/ncsu/calorietracker/TextViewActivity.java
@@ -3,15 +3,24 @@ package edu.ncsu.calorietracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class TextViewActivity extends AppCompatActivity {
 
+    private Button b1;
+    Button button;
+    EditText Input1;
+    TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_view);
-
+        Input1 = (EditText) findViewById(R.id.editInput1);
+        Input1.getText().toString();
+        text1 = (TextView) findViewById(R.id.editInput1);
+        text1.setText("Your calories are:"+text1.getText().toString());
     }
 }
