b69e87f023213330d62fc94dfb5bf8d4cc0f694c
Rundi Liu <15775677930@163.com>
2021-09-20 16:04:20 -0400

    add increase/decrease button

---
+++ app/src/main/java/edu/ncsu/calorietracker/CalCaloriesPage/CalMainActivity.java
@@ -0,0 +1,91 @@
+package edu.ncsu.calorietracker.CalCaloriesPage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import edu.ncsu.calorietracker.CheckHealthPage.TextViewActivity;
+import edu.ncsu.calorietracker.MainActivity;
+import edu.ncsu.calorietracker.R;
+
+public class CalMainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener{
+
+    private Button btn_increase;
+    private Button btn_decrease;
+    private EditText amount;
+    private Button btn_submit;
+    private int sum=0;
+    private int user_calories = 0;
+    private float x_submit = 100;
+    private float y_submit = 250;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal_main);
+        amount = (EditText) findViewById(R.id.et_amount);
+        this.btn_submit = (Button) findViewById(R.id.bt_cal) ;
+
+        this.btn_increase = (Button) findViewById(R.id.btn_increase);
+        this.btn_decrease = (Button) findViewById(R.id.btn_decrease);
+
+
+
+        this.btn_increase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sum++;
+                amount.setText(sum+"");
+                amount.setVisibility(View.VISIBLE);
+                btn_decrease.setVisibility(View.VISIBLE);
+            }
+        });
+        this.btn_decrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sum>0){
+                    sum--;
+                    amount.setText(sum+"");
+                    amount.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        if(editable.toString()==null)
+            return;
+        sum = Integer.valueOf(editable.toString());
+        if(sum > 10){
+            amount.setText("10");
+            return;
+        }
+    }
+
+    public int getSum(){
+        return sum;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
--- app/src/main/java/edu/ncsu/calorietracker/MainActivity.java
+++ app/src/main/java/edu/ncsu/calorietracker/MainActivity.java
@@ -1,27 +1,47 @@
 package edu.ncsu.calorietracker;
 
+import static android.view.View.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import edu.ncsu.calorietracker.CalCaloriesPage.CalMainActivity;
 import edu.ncsu.calorietracker.CheckHealthPage.TextViewActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button mbt_textView;
+    private Button mbt_cal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.mbt_textView = (Button) findViewById(R.id.bt_textview);
+        this.mbt_cal = (Button) findViewById(R.id.bt_cal) ;
+        /**
+        mbt_textView.setBackgroundColor(Color.WHITE);
+        mbt_textView.setTextColor(Color.BLACK);
+        mbt_cal.setBackgroundColor(Color.WHITE);
+        mbt_cal.setTextColor(Color.BLACK);
+         */
         this.mbt_textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // jump to new page (TextView)
                 Intent i = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(i);
+            }
+        });
+        this.mbt_cal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // jump to new page (TextView)
+                Intent i = new Intent(MainActivity.this, CalMainActivity.class);
                 startActivity(i);
             }
         });
