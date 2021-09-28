ca694222fb8789bebe6a456e1c2b32e32dc9c490
zhijin44 <zhijinyang@outlook.com>
2021-09-27 22:24:30 -0400

    add calorie input text dialog

--- app/src/main/java/edu/ncsu/calorietracker/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/edu/ncsu/calorietracker/ui/dashboard/DashboardFragment.java
@@ -1,16 +1,24 @@
 package edu.ncsu.calorietracker.ui.dashboard;
 
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import edu.ncsu.calorietracker.R;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import edu.ncsu.calorietracker.databinding.FragmentDashboardBinding;
 import edu.ncsu.calorietracker.viewmodel.DashboardViewModel;
@@ -28,6 +36,22 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        binding.btn.setOnClickListener(view -> {
+            new MaterialAlertDialogBuilder(getContext())
+                    .setTitle("Calorie：")
+                    .setView(R.layout.input_cal)
+                    .setPositiveButton(
+                            "ok",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    TextView input = ((AlertDialog) dialog).findViewById(android.R.id.text1);
+                                    Toast.makeText(getContext(), input.getText(), Toast.LENGTH_LONG).show();
+                                }
+                            })
+                    .setNegativeButton("cancel", null)
+                    .show();
+        });
 //        final TextView textView = binding.textDashboard;
 //        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
 //            @Override
